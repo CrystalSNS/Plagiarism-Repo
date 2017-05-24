22fda88725722e2dc12b3159d248e56ceb7047f1
Sreynoch Soung <noch@getalp-adaptateur-reseau.imag.fr>
2017-05-24 20:01:21 +0200

    - Update function in Classifier Class

--- PlagiarismDetection/src/main/ClassifierCl.java
+++ PlagiarismDetection/src/main/ClassifierCl.java
@@ -2,9 +2,13 @@ package main;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import edu.stanford.nlp.util.ArrayUtils;
 import weka.classifiers.Classifier;
@@ -15,6 +19,7 @@ import weka.classifiers.trees.RandomForest;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
 import weka.filters.unsupervised.instance.RemoveRange;
 
 
@@ -57,6 +62,7 @@ public class ClassifierCl {
 		writer.close();
 	}
 
+	
 	public void learnClassifierIncremental() throws Exception {
 
 		File folder = new File("result/train/1train-pan11/part1");
@@ -118,13 +124,13 @@ public class ClassifierCl {
 
 	}
 
-	Instances mergedDataInst;
-	public List<Integer> loadIndex(String pt) throws Exception {
+	
+	public void mergeArff(String pt) throws Exception {
 
 		List<Integer> indexArr = new ArrayList<Integer>();
 		File file = new File(pt);
 		File[] listOfFile = file.listFiles();
-		
+
 		for (int j = 0; j < listOfFile.length; j++) {
 			if(!listOfFile[j].getName().endsWith(".arff")){
 				listOfFile =(File[]) ArrayUtils.removeAt(listOfFile,j);
@@ -135,10 +141,30 @@ public class ClassifierCl {
 		Instances tmpInst ;
 		
 		tmpLoader = new ArffLoader();
-		tmpLoader.setFile(new File(pt+"/"+listOfFile[0].getName()));
-		mergedDataInst = tmpLoader.getDataSet();
+//		--------
+//		tmpLoader.setFile(new File(pt+"/"+listOfFile[0].getName()));
 		
-		int begin =1, end=0;
+//		-----------------------------------------------------
+//		merge with the old file => then get out the condition if(j!=0) in loop
+		tmpLoader.setFile(new File("result/mergedDataInst.arff"));
+//		-----------------------------------------------------
+		
+		Instances mergedDataInst = tmpLoader.getDataSet();
+		
+//		-----------------------------------------------------
+//		get last index from old file
+		
+		Scanner s = new Scanner(new File("result/index.txt"));
+		ArrayList<Integer> indexArrOld = new ArrayList<Integer>();
+		while (s.hasNext()){
+			indexArrOld.add(s.nextInt());
+		}
+		s.close();
+		int begin = indexArrOld.get(indexArrOld.size() - 1) + 1, end = indexArrOld.get(indexArrOld.size() - 1);
+		
+		
+//		-----------------------------------------------------
+//		int begin = 1, end = 0;
 		
 		if (listOfFile.length != 0) {
 			for (int j = 0; j < listOfFile.length; j++) {
@@ -149,20 +175,38 @@ public class ClassifierCl {
 				indexArr.add(begin);
 				indexArr.add(end);
 				begin =  end + 1;
-				if(j!=0){
+//				if(j != 0){
 					mergedDataInst.addAll(tmpInst);
-				}
+					System.out.println("..merging file:" +j);
+//				}
 			}
 		}
+		
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(mergedDataInst);
+		try {
+			saver.setFile(new File("result/mergedDataInst.arff"));
+			saver.writeBatch();
+			System.out.println("Saved file arff: result/mergedDataInst.arff");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+//		PrintWriter pw = new PrintWriter(new FileOutputStream(new File("result/index.txt")));
+		PrintWriter pw = new PrintWriter(new FileOutputStream(new File("result/index.txt"), true));
+	    for (Integer index : indexArr)
+	        pw.println(index.toString());
+	    pw.close();
+	    System.out.println("Saved file txt: result/index.txt");
+		
 		System.out.println("Total number of instances:"+mergedDataInst.numInstances());
 		System.out.println("Done load index and Merged Data !!");
-		return indexArr;
 	}
 	
 	
-	public void voteClassifier(String ptFolder) throws Exception {
+	public void voteClassifier(String ptData, String ptIndex) throws Exception {
 
-		List<Integer> indexArr = loadIndex(ptFolder);
 		int begin,end;
 
 		List<Classifier> classifiersArr = new ArrayList<Classifier>();
@@ -173,6 +217,13 @@ public class ClassifierCl {
 		RemoveRange rmRange = new RemoveRange();
 		FilteredClassifier fc;
 		
+		Scanner s = new Scanner(new File(ptIndex));
+		ArrayList<Integer> indexArr = new ArrayList<Integer>();
+		while (s.hasNext()){
+			indexArr.add(s.nextInt());
+		}
+		s.close();
+
 		for(int i= 0; i < indexArr.size(); i=i+2){
 			
 			begin = indexArr.get(i);
@@ -188,18 +239,23 @@ public class ClassifierCl {
 
 			classifiersArr.add(fc);
 		}
-		System.out.println("Number of Classifier: " + classifiersArr.size());
-		mergedDataInst.setClassIndex(mergedDataInst.numAttributes() - 1);
-
+		System.out.println("Number of Classifiers: " + classifiersArr.size());
+		ArffLoader tmpLoader = new ArffLoader();
+		tmpLoader.setFile(new File(ptData));
+		Instances DataInst = tmpLoader.getDataSet();
+		DataInst.setClassIndex(DataInst.numAttributes() - 1);
+		
 		Vote voter = new Vote();
 
 		voter.setClassifiers(classifiersArr.toArray(new Classifier[classifiersArr.size()]));
-		voter.buildClassifier(mergedDataInst);
+		voter.buildClassifier(DataInst);
 		
 		weka.core.SerializationHelper.write("result/model/voterRF.model", voter);
 		
 		System.out.println("Saved model!");
+		System.out.println();
 	}
+	
 	
 	public void renameFile(String pt) {
 		
@@ -208,7 +264,8 @@ public class ClassifierCl {
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".arff")) {
                 File f = new File(pt + "/" + listOfFiles[i].getName()); 
-                f.renameTo(new File(pt+"/test11-" + listOfFiles[i].getName() ));
+//                f.renameTo(new File(pt+"/test11-" + listOfFiles[i].getName() ));
+                f.renameTo(new File(pt+"/" + listOfFiles[i].getName().substring(7,listOfFiles[i].getName().length()) ));
             }
         }
         System.out.println("conversion is done");
--- PlagiarismDetection/src/main/MainCl.java
+++ PlagiarismDetection/src/main/MainCl.java
@@ -3,24 +3,29 @@ package main;
 public class MainCl {
 
 	public static void main(String[] args) {
-//		TextCl text = new TextCl();
+		TextCl text = new TextCl();
 //		text.extractTextAndGroundTruth("corpus/1train-pan11/part" + args[0], true);
 //		text.extractTextAndGroundTruth("corpus/1train-pan11/part26-" + args[0], true);
 //		text.extractTextAndGroundTruth("corpus/2test-pan11/suspisious-docs", true);
 //		text.extractTextAndGroundTruth("corpus/2test-pan11/suspicious-docs-"+ args[0] , true);
 //		text.extractTextAndGroundTruth("corpus/3train-pan16/suspisious-docs", true);
-//		text.extractTextAndGroundTruth("corpusTest/2test-pan11/suspicious-docs", false);
+		text.extractTextAndGroundTruth("corpusTest/3train-pan16/suspisious-docs", false);
 
-		ClassifierCl classifier = new ClassifierCl();
-		try {
+//		ClassifierCl classifier = new ClassifierCl();
+//		try {
 //			classifier.learnClassifier();
 //			classifier.learnClassifierIncremental();
+			
+//			classifier.mergeArff("corpusTrain/test1");
+//			classifier.mergeArff("corpusTrain/mergTrainingData");
 
-//			classifier.voteClassifier("corpusTrain/test");
-			classifier.renameFile("corpusTrain/test");
-//			classifier.voteClassifier("corpusTrain/mergTrainingData");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//			classifier.renameFile("corpusTrain/test");
+//			classifier.renameFile("corpusTrain/mergTrainingData");
+			
+//			classifier.voteClassifier("result/mergedDataInst.arff","result/index.txt");
+			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 }
